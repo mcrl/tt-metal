c7c6c2107aef495a64c5cbd7c2daa39fdd39671a
Stanislav Minakov <sminakov@tenstorrent.com>
2025-08-13 12:30:05 +0000

    Fix CI: Use Rustup to install newer Rust version fixing the build (#26756)
    ### Ticket
    
    ### Problem description
    The build on CI is currently broken with the following error:
    ```
    error: package `rayon-core v1.13.0` cannot be built because it requires rustc 1.80 or newer, while the currently active rustc version is 1.75.0
    Either upgrade to rustc 1.80 or newer, or use
    cargo update rayon-core@1.13.0 --precise ver
    where `ver` is the latest version of `rayon-core` supporting rustc 1.75.0
    ```
    
    ### What's changed
    Install the newer version of Rust in CI docker containers using Rustup
    (the recommended method)
    Updated tokenizers-cpp dependency to fix build issues
    
    Future work is required to prevent this issue from happening in the
    future and will be done in a separate (non urgent) PR
    
    ### Checklist
    - [x] [All post commit CI build
    passes](https://github.com/tenstorrent/tt-metal/actions/runs/16934398888)
    - [x] New/Existing tests provide coverage for changes

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -207,7 +207,6 @@ init_packages() {
                 "libc++abi-17-dev"
                 "wget"
                 "curl"
-                "xxd"
             )
             if [ "$distributed" -eq 1 ]; then
                 PACKAGES+=("openmpi-bin" "libopenmpi-dev")
@@ -235,7 +234,6 @@ init_packages() {
                 "capstone-devel"
                 "wget"
                 "curl"
-                "vim-common" # Includes xxd
             )
             if [ "$distributed" -eq 1 ]; then
                 PACKAGES+=("openmpi" "openmpi-devel")
@@ -407,12 +405,7 @@ install_mpi_ulfm() {
 }
 
 install_rust() {
-    INSTALL_CMD="curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain 1.89.0 --profile minimal -y"
-    if [ -n "$SUDO_USER" ]; then
-        sudo -u "$SUDO_USER" /bin/bash -c "$INSTALL_CMD"
-    else
-        /bin/bash -c "$INSTALL_CMD"
-    fi
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain 1.89.0 -y
 }
 
 # We don't really want to have hugepages dependency
