9ce993a410d59cbc16238956e3e2e7d726971f43
Jinpyo Kim <jinpyo@aces.snu.ac.kr>
2025-09-22 13:13:20 +0900

    Adapt model cache.

--- models/demos/qwen3/tt/rms_norm.py
+++ models/demos/qwen3/tt/rms_norm.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 import ttnn
+from pathlib import Path
 
 
 class Qwen3MoeRMSNorm(nn.Module):
@@ -11,17 +12,20 @@ class Qwen3MoeRMSNorm(nn.Module):
         self.variance_epsilon = eps
 
     def setup_tt(self):
-        self.weight_tt = ttnn.from_torch(
+        self.weight_tt = ttnn.as_tensor(
             self.weight,
             dtype=ttnn.bfloat16,
             layout=ttnn.TILE_LAYOUT,
             device=self.mesh_device,
             memory_config=ttnn.DRAM_MEMORY_CONFIG,
             mesh_mapper=ttnn.ReplicateTensorToMesh(self.mesh_device),
+            cache_file_name=Path.home() / ".cache/weights" / f"rmsnorm_{id(self)}_weight",
         )
 
     def forward(self, hidden_states: ttnn.Tensor) -> ttnn.Tensor:
-        return ttnn.rms_norm(hidden_states, epsilon=self.epsilon, weight=self.weight_tt, memory_config=ttnn.L1_MEMORY_CONFIG)
+        return ttnn.rms_norm(
+            hidden_states, epsilon=self.epsilon, weight=self.weight_tt, memory_config=ttnn.L1_MEMORY_CONFIG
+        )
 
 
 __all__ = ["Qwen3MoeRMSNorm"]
